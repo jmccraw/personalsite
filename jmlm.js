852f5bef2ddc40e2806a4121bdc2b52b23ec4614
Justin McCraw <jmccraw1@gmail.com>
2014-09-23 08:54:53 -0400

    Updated home page styles and designs

---
+++ jmlm.js
@@ -0,0 +1,10 @@
+/**
+ * Created by justinmccraw on 7/19/14.
+ */
+(function() {
+  "use strict";
+
+  setTimeout(function() {
+    document.getElementsByTagName("main")[0].className = "signature";
+  }, 1200);
+})();
