513219ca74240c55dde2d78280405bee88657f9b
Justin McCraw <jmccraw1@gmail.com>
2014-09-28 13:23:44 -0400

    Updated transition stuff, but actually need webkitAnimationEnd, which doesn't transition on the final one in the chain?

--- jmlm.js
+++ jmlm.js
@@ -4,7 +4,56 @@
 (function() {
   "use strict";
 
-  setTimeout(function() {
+  var transition = "";
+
+  /**
+   * Finds the transitionend event to use for this browser
+   * Slightly modified from webinista: http://stackoverflow.com/a/9090128/1472477
+   */
+  function checkTransitionSupport() {
+    var transitions = {
+      "transition": "transitionend",
+      "WebkitTransition": "webkitTransitionEnd",
+      "OTransition": "otransitionend",
+      "MozTransition": "transitionend"
+    };
+    var el = document.createElement("div");
+
+    for (var i in transitions) {
+      if (transitions.hasOwnProperty(i) && el.style[i] !== void 0) {
+        transition = transitions[i];
+        break;
+      }
+    }
+
+    if (transition === "") {
+      transition = "nosupport";
+    }
+  }
+
+  /**
+   * Reveal the signature after the flashing stops
+   */
+  function revealSignature() {
     document.getElementsByTagName("main")[0].className = "signature";
-  }, 1200);
+  }
+
+  /**
+   * Initialize the JavaScript that needs it
+   */
+  function init() {
+    checkTransitionSupport();
+
+    // add event support for the transition ending
+    if (transition === "nosupport") {
+      document.body.addEventListener(transition, revealSignature, false);
+    }
+    else {
+      setTimeout(function() {
+        document.getElementsByTagName("main")[0].className = "signature";
+      }, 1200);
+    }
+  }
+
+  init();
 })();
